ddbaf0420021ac8b5f17c901cfce138fcafb295d
Rajashekar Kunta <45508221+Kuntarajashekar@users.noreply.github.com>
2024-09-17 19:58:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,8 +94,8 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-pro-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
