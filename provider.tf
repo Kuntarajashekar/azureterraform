da7b9315b1cd8952fccf4c2941249237cf9b1416
Rajashekar Kunta <45508221+Kuntarajashekar@users.noreply.github.com>
2024-12-16 20:37:52 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  # subscription_id = "eaf80817-a1b9-4dec-9510-6c4ff8eab91d"
-  #client_id       = "caea0a12-930e-4f5c-974d-42f9d45ea970"
-  #client_secret   = "r7e8Q~1KxDkv02L0yEJ_1mxq7G6Qjs2xowYyWbwO"
-  #tenant_id       = "da1800a8-f3b6-4ac9-b925-49181c603ae0"
+  # subscription_id = ""
+  #client_id       = ""
+  #client_secret   = ""
+  #tenant_id       = ""
   features {}
 }
 
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.0.0"
+      version = "5.0.0"
     }
   }
 }
