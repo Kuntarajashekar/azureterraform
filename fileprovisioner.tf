ed05808af452508f7b9137a31ba3834cd0200b42
Rajashekar Kunta <45508221+Kuntarajashekar@users.noreply.github.com>
2024-12-28 15:01:57 -0500

    Create fileprovisioner.tf

---
+++ fileprovisioner.tf
@@ -0,0 +1,118 @@
+#create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "Landingzone"
+  location = "canadacentral"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Landing-vnet"
+  address_space       = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/27"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "public_ip" {
+  name                = "Landing-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "Landing-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "Landing-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "myNicConfiguration"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "linuxvm" {
+  name                            = "Landing-vm"
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+  size                            = "Standard_B1s"
+  admin_username                  = "azureuser"
+  admin_password                  = "Azureadmin@12345"
+  disable_password_authentication = false
+
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+resource "null_resource" "file_provisioner" {
+  depends_on = [azurerm_linux_virtual_machine.linuxvm]
+
+  provisioner "file" {
+    source      = "/home/raghuram/terraform/raja" # Path to the local file
+    destination = "/home/azureuser/raja"          # Path on the remote server
+
+    connection {
+      type     = "ssh"
+      host     = azurerm_public_ip.public_ip.ip_address
+      user     = "azureuser"
+      password = "Azureadmin@12345"
+    }
+  }
+}
+
